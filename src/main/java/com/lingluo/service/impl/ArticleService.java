ddc1c876e1d7dfa42312b996a5d2eb6a68687108
laiwei <184440579@qq.com>
2019-04-01 21:42:30 +0800

    update

---
+++ src/main/java/com/lingluo/service/impl/ArticleService.java
@@ -0,0 +1,14 @@
+package com.lingluo.service.impl;
+
+import com.lingluo.model.Article;
+import com.lingluo.service.IArticleService;
+
+public class ArticleService implements IArticleService {
+
+	@Override
+	public Article getArticleById(String id) {
+	
+		return null;
+	}
+
+}
