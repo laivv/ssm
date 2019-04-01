b11a76effe113fad67cbd58e0001880f79f757fb
laiwei <184440579@qq.com>
2019-04-01 21:42:06 +0800

    update

--- src/main/java/com/lingluo/controller/HomeController.java
+++ src/main/java/com/lingluo/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.lingluo.controller;
 
 import java.io.IOException;
 
+import javax.naming.Context;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,10 +10,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.aspectj.weaver.ast.Var;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 @Controller
 public class HomeController {
-
+	//ApplicationContext context= new ClassPathXmlApplicationContext("./applicationContext.xml");
 	@RequestMapping("/index.do")
 	public void index(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.sendRedirect("/index.jsp");
