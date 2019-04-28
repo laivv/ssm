afbc1b75870aab718fbdd9accb30063e472080ab
laiwei <184440579@qq.com>
2019-04-28 23:02:20 +0800

    update

--- src/main/java/com/lingluo/controller/ArticleController.java
+++ src/main/java/com/lingluo/controller/ArticleController.java
@@ -1,18 +1,16 @@
 package com.lingluo.controller;
 
-import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.io.IOException;
-import java.sql.Timestamp;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.lingluo.common.Page;
 import com.lingluo.common.PageResult;
 import com.lingluo.common.Result;
@@ -39,19 +37,20 @@ public class ArticleController {
 	}
 	
 	@RequestMapping(value="/article/{id}", method = RequestMethod.GET)
-	public Article getArticle(@PathVariable String id) throws IOException{
+	public Result<Article> getArticle(@PathVariable String id) throws IOException{
 		Article article = articleService.findById(id);
-		return article;
+		Result<Article> result = new Result<Article>(article);
+		return result;
 	}
 	
 	
-	@RequestMapping(value="/article/add",method = RequestMethod.GET)
-	public String addArticle() throws IOException{
+	@RequestMapping(value="/article",method = RequestMethod.PUT)
+	public Result<Object> addArticle() throws IOException{
 		Article article = new Article();
 		article.setTitle("测式");
 		article.setCommentState(1);
 		article.setContent("测试内容");
-		Timestamp d = new Timestamp(System.currentTimeMillis());
+		Date d = new Date(System.currentTimeMillis());
 		article.setCreateDate(d);
 		article.setCreateUser(1);
 		article.setPermission(0);
@@ -59,14 +58,37 @@ public class ArticleController {
 		article.setVisitCount(0);
 		article.setCategoryId(1);
 		articleService.save(article);
-		return "1";
-		
+		return new Result<Object>(null);
 	}
-	
 	
 	@RequestMapping(value="/article",method = RequestMethod.POST)
-	public String updateArticle(@RequestParam String id) throws IOException{
-		return "1";
+	public Result<Object> updateArticle(@RequestBody Article article) throws IOException{
+		articleService.update(article);
+		return new Result<Object>(null);
 	}
+	
+	
+	@RequestMapping(value="/article/{id}",method = RequestMethod.DELETE)
+	public Result<Object> deleteArticle(@PathVariable String id) throws IOException{
+		articleService.delete(id);
+		return new Result<Object>(null);
+	}
+	
+	@RequestMapping(value="/article/add",method = RequestMethod.GET)
+	public String addArticleTest() throws IOException{
+		addArticle();
+		return "添加成功";
+	}
+	
+	@RequestMapping(value="/article/delete/{id}",method = RequestMethod.GET)
+	public String deleteArticleTest(@PathVariable String id) throws IOException{
+		deleteArticle(id);
+		return "删除成功";
+	}
+	
+//	@RequestMapping(value="/article",method = RequestMethod.POST)
+//	public String updateArticle(@RequestParam String id) throws IOException{
+//		return "1";
+//	}
 	
 }
---
+++ src/main/java/com/lingluo/dto/ArticleDto.java
@@ -0,0 +1,8 @@
+package com.lingluo.dto;
+
+import java.util.Date;
+
+public class ArticleDto {
+
+	
+}
